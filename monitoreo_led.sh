d61a38106efaf30e277a4ca8c569ddfd7a23a1af
asron <aaron.munozwright@ucr.ac.cr>
2025-06-10 20:23:10 +0000

    Update monitoreo_led.sh

--- monitoreo_led.sh
+++ monitoreo_led.sh
@@ -14,18 +14,17 @@ else
 fi
 
 PID="" # esto va a almacenar el pid del proceso que abra el archivo
-contador_open=0
+
 while true; do #esto crea un bucle que esta revisando coninuamente si el archivo es abierto o cerrado
 
 
 
-EVENTO=$(inotifywait -t 3  --format '%e' "$FILE" 2>/dev/null) #cada segundo verifica si hay un proceso
+EVENTO=$(inotifywait -t 1  --format '%e' "$FILE" 2>/dev/null) #cada segundo verifica si hay un proceso
 
 if [ -z "$EVENTO" ]; then #si evento está vacío, verifica si hay un pid guardado
     if [ -n "$PID" ]; then #si hay un pid guardado, verifica si el proceso está activo
         if ! ps -p "$PID" > /dev/null; then #si el proceso no está activo, apaga el led
             echo "El proceso $PID no está activo, apagar LED"
-            contador_open=0
             PID="" #esto limpia el pid
 
             #aqui deberia invocar el comando que diga al kernel que apague el led
@@ -35,33 +34,25 @@ if [ -z "$EVENTO" ]; then #si evento está vacío, verifica si hay un pid guarda
 else #si hay un evento, lo imprime y lo procesa
     echo "$EVENTO"
     if [[ "$EVENTO" == *"OPEN"* ]]; then #si el archivo esta abierto...
-        PID=$(lsof "$FILE" | awk 'NR==2 {print $2}') #esto guarda el pid del proceso
-        contador_open=$((contador_open + 1))
-        if [[ "$contador_open" -eq 1 ]]; then #la idea es que si detectmos dos open seguidos quiere decir que guardó
-            echo "Archivo abierto"
+        sleep 0.6
+        PID=$(lsof -t "$FILE" | head -n 1) #esto guarda el pid del proceso
+            echo "Archivo abierto con el proceso $PID"
 
             #aqui deberia invocar el comando que diga al kernel que encienda el led
 
-        else
-            echo "proceso terminado "
-
-            #aqui deberia invocar el comando que diga al kernel que apague el led
-        fi
-
     elif [[ "$EVENTO" == *"ACCESS"* ]]; then #si el archivo fue abierto...
-        #echo "Archivo abierto"
+        echo "Archivo abierto"
         
         #aqui deberia invocar el comando que diga al kernel que encienda el led
 
     elif [[ "$EVENTO" == *"CLOSE_WRITE"* ]]; then #si el archivo fue cerrado despues de modificar...
         echo "Archivo cerrado" 
-        contador_open=0
+
         #aqui deberia invocar el comando que diga al kernel que apague el led
 
     elif [[ "$EVENTO" == *"ATTRIB"* ]]; then #si el archivo fue cerrado despues de modificar...
         echo "Archivo guardado" 
-        contador_open=0
-
+        
         #aqui deberia invocar el comando que diga al kernel que apague el led
 
 
@@ -69,4 +60,5 @@ else #si hay un evento, lo imprime y lo procesa
 
 fi
 done
+
 
