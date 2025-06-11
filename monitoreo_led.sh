8b6ddfe3bd1390323b982660ad264087cbaeb332
asron <aaron.munozwright@ucr.ac.cr>
2025-06-11 22:38:06 +0000

    Update monitoreo_led.sh

--- monitoreo_led.sh
+++ monitoreo_led.sh
@@ -2,25 +2,18 @@
  
 #este archivo debe tener permisos de ejecucion (chmod +x monitoreo_led.sh)
 
+FILE=/home/vboxuser/Escritorio/monitoreo_led.txt #esta ruta depende de cada equipo, reemplazar por la ruta al escritorio de cada equipo
+EDITOR="mousepad" #todo el proyecto esta pensado para funcionar con mousepad
+
+command -v inotifywait >/dev/null || { echo "inotifywait no está instalado"; exit 1; } #esto avisa si falta alguna dependencia
+command -v pgrep >/dev/null || { echo "pgrep no está instalado"; exit 1; }
+
 trap 'terminar' SIGINT SIGTERM #esto llama la funcion terminar al detener el script
 terminar() { #esta funcion termina el subproceso verificar_proceso
     echo " Saliendo..."
     kill "$PID_VERIFICADOR" 2>/dev/null
-    kill "$PID_BPFTRACE" 2>/dev/null
     exit
 }
-
-FILE=/home/vboxuser/Escritorio/monitoreo_led.txt #esta ruta depende de cada equipo, reemplazar por la ruta al escritorio de cada equipo
-EDITOR="mousepad"
-#lo primero es saber si existe el archivo de monitoreo, esto se va a repetir en bucle. por lo que deberia crear el archivo solo la primera iteracion 
-if [ -f "$FILE" ]; then
-    echo "archivo encontrado" #esto no deberia aparecer en la version final
-else
-    echo "El archivo no existe" #si el archivo no existe, lo creamos
-    touch "$FILE"
-    echo "$FILE creado"
-fi
-
 
 encender_led(){
   echo "led encendido"
@@ -32,6 +25,17 @@ apagar_led(){
     #aqui deberia invocar el comando que diga al kernel que apague el led
 }
 
+parpadeo(){
+    tempo=0.3
+    apagar_led
+    sleep $tempo
+    encender_led
+    sleep $tempo
+    apagar_led
+    sleep $tempo
+    encender_led
+}
+
 verificar_proceso() {
     while true; do
         # Busca el PID del editor con ese archivo abierto
@@ -39,13 +43,14 @@ verificar_proceso() {
         
         if [[ -n "$pid" ]]; then
             if [[ "$EDITOR_PID" != "$pid" ]]; then
-                echo "Editor $EDITOR abierto con $FILE (PID $pid)"
-                EDITOR_PID="$pid"
+                echo "-Editor $EDITOR abierto con $FILE (PID $pid)"
                 encender_led
+                EDITOR_PID="$pid"
             fi
         else
             if [[ -n "$EDITOR_PID" ]]; then
-                echo "Editor $EDITOR cerrado o no con $FILE"
+                sleep 1
+                echo "-Archivo cerrado"
                 EDITOR_PID=""
                 apagar_led
             fi
@@ -53,57 +58,27 @@ verificar_proceso() {
         sleep 1
     done
 }
-PID="" # esto va a almacenar el pid del proceso que abra el archivo
+
+
+#lo primero es saber si existe el archivo de monitoreo, esto se va a repetir en bucle. por lo que deberia crear el archivo solo la primera iteracion 
+if [ -f "$FILE" ]; then
+    echo "archivo encontrado" #esto no deberia aparecer en la version final
+else
+    echo "El archivo no existe" #si el archivo no existe, lo creamos
+    touch "$FILE"
+    echo "$FILE creado"
+fi
+
 verificar_proceso & #esto llama a la funcion verificar proceso a trabajar en segundo plano
 PID_VERIFICADOR=$! #esto guarda el pid del subproceso
-
-exec 3< <(  #esto es un subproceso en segundo plano que registra el proceso que abre el archivo
-    
-    sudo bpftrace -e "
-    tracepoint:syscalls:sys_enter_openat
-    /str(args->filename) == \"$FILE\"/
-    {
-        printf(\"%d\\n\", pid);
-    }" 2>/dev/null
-)
-PID_BPFTRACE=$!
 
 
 while true; do #esto crea un bucle que esta revisando coninuamente si el archivo es abierto o cerrado
 EVENTO=$(inotifywait --format '%e' --event open,close_write,attrib "$FILE" 2>/dev/null) 
-
-
-
- #si hay un evento, lo imprime y lo procesa
-    echo "$EVENTO"
-    
-    if [[ "$EVENTO" == *"OPEN"* ]]; then #si el archivo fue abierto...
-        while read -t 1 -u 3 line; do #toma el proceso que lo abre
-            if [[ "$line" =~ ^[0-9]+$ ]]; then
-                PID="$line"
-                break
-            fi
-        done
-        if [ -n "$PID" ]; then 
-            editor=$(ps -p "$PID" -o comm=)
-            if [[ "$editor" == "$EDITOR" ]]; then
-                if [ "$EDITOR_PID" != "$PID" ]; then
-                    echo "Archivo abierto con $EDITOR (PID $PID)"
-                    EDITOR_PID="$PID"
-                    encender_led
-                fi
-            else
-                echo "Archivo abierto por otro proceso ($editor), ignorado"
-            fi
-        fi
-       
-    elif [[ "$EVENTO" == *"CLOSE_WRITE"* ]]; then #si el archivo fue cerrado despues de modificar...
-        echo "Archivo cerrado" 
-        apagar_led
-
-    elif [[ "$EVENTO" == *"ATTRIB"* ]]; then #si el archivo fue cerrado despues de modificar...
-        echo "Archivo guardado" 
-        apagar_led
+ #si hay un evento, lo imprime y lo procesa  
+    if [[ "$EVENTO" == *"ATTRIB"* ]]; then #si el archivo fue cerrado despues de modificar...
+        echo "-Archivo guardado" 
+        parpadeo
 
     fi #cierra el if
 done
